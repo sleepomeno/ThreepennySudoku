e75c18fb22d71d892b0043294473ab30ce710b69
Gregor Riegler <gregor.riegler@gmail.com>
2014-06-13 18:43:01 +0200

    Add solvedPanel. Stop/Start timer when appropriate

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -14,7 +14,7 @@ import Graphics.UI.Threepenny.Core
 import Paths
 import Reactive.Threepenny
 
-import Data.List(groupBy,sort,isPrefixOf)
+import Data.List(foldl1', groupBy,sort,isPrefixOf)
 import Data.Array.IArray
 import Data.List.Split
 import Data.Char(isDigit)
@@ -24,7 +24,7 @@ import System.FilePath((</>),(<.>))
 import Control.Monad(when,join,void,liftM,sequence,forM)
 import Control.Monad.Trans.Class
 import Data.Time
-import Control.Arrow(first,second,(***))
+import Control.Arrow(first,second,(***),(+++),(&&&),(>>>))
 import Control.Monad.Trans.State as S
 import Control.Lens ((^.),(&),Lens', makeLenses, view)
 import Control.Lens.Getter (use)
@@ -107,8 +107,19 @@ zipLength4 ~[a,b,c,d] ~[e,f,g,h] ~[i,j,k,l] = [(a,e,i),(b,f,j),(c,g,k),(d,h,l)]
 findSudoku :: [SudokuWithId] -> Integer -> SudokuWithId
 findSudoku sudokus sid = head $ filter (\(Sudoku sid' _ _ ) -> sid' == sid) sudokus 
 
+findSudoku' :: [SudokuWithId] -> Integer -> Maybe SudokuWithId
+findSudoku' sudokus sid = case filter (\(Sudoku sid' _ _ ) -> sid' == sid) sudokus of
+  [] -> Nothing
+  (a:as) -> Just a
+
 chooseSudokus = "--- choose a Sudoku ---"
 filterSudokusE = filterE (/= chooseSudokus) 
+
+eNewDigit :: [[SudokuWithId]] -> Event String -> (Int, Int) -> Event (Maybe Digit)
+eNewDigit sudokus eSID (col, row) = 
+  let eSudokuWithId = findSudoku' (concat sudokus) . read <$> eSID
+  in
+  fmap ((!!col) . (!!row) . chunksOf 9 . extractDigits ) `fmap` eSudokuWithId
 
 runApp :: LabeledSudokus -> Window -> UI ()
 runApp labeledSudokus w = void $ mdo
@@ -128,17 +139,17 @@ runApp labeledSudokus w = void $ mdo
   let bSudoku = fmap (findSudoku (concat sudokus) . read) <$> bListBox :: Behavior (Maybe SudokuWithId)
 
   -- Create cells and corresponding mistake event streams
-  cells <- sequence [createCell col row' digit eSudoku | row' <- [0..8], col <- [0..8],
-                     let digit  = fmap ((!!col) . (!!row') . chunksOf 9 . extractDigits ) `fmap` bSudoku ] 
-  let (cellElems, eCorrectnessEvents) = unzip $ fmap (first element) cells :: ([UI Element], [Event Bool])
+  cells <- sequence [createCell col row' eSudoku eNewDigit' | row' <- [0..8], col <- [0..8],
+                     let eNewDigit' = eNewDigit sudokus eSudoku (col, row')] 
+  let (cellElems, eDigitCorrect, eCorrect) = unzip3 cells :: ([Element], [Event Bool], [Event Bool])
 
   -- Create content panels
   let bChoose = (\x -> if isJust x then "welcome hide" else "welcome") <$> bListBox 
       bSudokuGrid = (\x -> if isJust x then "sudoku" else "sudoku hide") <$> bListBox
   choosePanel <- UI.div #. "welcome" #+ [UI.h2 # set text "Choose a Sudoku ->"]
-  element choosePanel # sink UI.class_ bChoose
+  widget choosePanel # sink UI.class_ bChoose
   contentPanel <- UI.div
-  element contentPanel # sink UI.class_ bSudokuGrid #+ sudokuGrid cellElems
+  widget contentPanel # sink UI.class_ bSudokuGrid #+ sudokuGrid cellElems
   
   -- create wrappers for select boxes
   -- make sure the wrapper for the level of the current sudoku gets highlighted
@@ -148,34 +159,36 @@ runApp labeledSudokus w = void $ mdo
              let otherEvents = take nr eListBoxes ++ drop (nr+1) eListBoxes
                  ownEvent = eListBoxes!!nr
                  containerPanel = UI.div
-             -- if a sudoku of other select box gets selected then the classs is "selectSudokus"
-             -- if a sudoku of own select box gets selected then the classs is "selectSudokus hightlighted"
+             -- if a sudoku of other select box gets selected then the class is "selectSudokus"
+             -- if a sudoku of own select box gets selected then the class is "selectSudokus hightlighted"
              bWrapperClass <- stepper "selectSudokus" $ head <$>
                               unions ["selectSudokus" <$ filterSudokusE (head <$> unions otherEvents),
                                       "selectSudokus highlighted" <$ filterSudokusE ownEvent]
-             containerPanel # sink UI.class_ bWrapperClass #+ [element level, (element . getElement) listBox]
+             containerPanel # sink UI.class_ bWrapperClass #+ [widget level, element listBox]
 
   -- sudoku caption
   captionPanel <- UI.h1
   let bCaption = maybe "Welcome to Sudoku Land!" ("Sudoku "++) <$> bListBox
-  element captionPanel # sink text bCaption
+  widget captionPanel # sink text bCaption
 
-  let eNotCorrectEvents = map (\x -> () <$ filterE not x) eCorrectnessEvents
-      eCorrectEvents = map (\x -> () <$ filterE id x) eCorrectnessEvents
-  bCorrectnessEvents <- (forM eCorrectnessEvents $ \x -> stepper False x) :: UI [Behavior Bool]
-  -- let bSudokuCorrect = and <$> sequence bCorrectnessEvents
-  -- TODO
+  let eIncorrectDigit = map (\x -> () <$ filterE not x) eDigitCorrect
+  -- bCorrectnessOfInput <- forM eCorrect $ \x -> stepper 0 (head <$> unions [((\x -> if x then 1 else 0) <$> x)])
+  -- bCorrectnessOfInput <- forM eCorrect $ stepper False 
+  bCorrectnessOfInput <- forM eCorrect $ stepper (0::Int) . fmap (\x -> if x then 1 else 0)
+  -- let bSudokuCorrect = foldl1' (\x y -> (&&) <$> x <*> y) bCorrectnessOfInput :: Behavior Bool
+  let bSudokuCorrect = foldl1' (\x y -> (+) <$> x <*> y) bCorrectnessOfInput
+      eSudokuCorrect = ((\x -> const (x>28)) <$> bSudokuCorrect) `filterApply` (head <$> unions eCorrect)
   
-  -- mistakes
-  mistakesPanel <- UI.h3
-  bMistakesInt <- accumB 0 $ concatenate <$> unions [
+  -- error
+  errorPanel <- UI.h3
+  bErrorInt <- accumB 0 $ concatenate <$> unions [
     const 0 <$ eSudoku,
-    -- (+1)  <$ (() <$ (fmap (filter not) $ unions eCorrectnessEvents))
-    (+1)  <$ unions eNotCorrectEvents
+    (+1)  <$ unions eIncorrectDigit
     ] :: UI (Behavior Int)
-  let bMistakes = (++ " Mistakes") . show <$> bMistakesInt
-  element mistakesPanel # sink text bMistakes
-  element mistakesPanel # sink UI.class_ (maybe "hide" (const "") <$> bListBox)
+  -- let bError = (++ " Error") . show <$> bErrorInt
+  let bError = (show &&& (\x -> if x == 1 then " Error" else " Errors") >>> uncurry (++)) <$> bErrorInt
+  widget errorPanel # sink text bError
+  widget errorPanel # sink UI.class_ (maybe "hide" (const "") <$> bListBox)
 
   -- timer
   timePanel <- UI.h3
@@ -184,34 +197,47 @@ runApp labeledSudokus w = void $ mdo
   eTimerInt <- accumE (0 :: Int) $ concatenate <$> unions [const 0 <$ eSudoku, (+1) <$ UI.tick timer] :: UI (Event Int)
   let padNumber x = if x < 10 then '0':show x else show x
       eTimerFormatted = ((\(x,y) -> x ++ ":" ++ y) . join (***) padNumber . (`quotRem` 60)) <$> eTimerInt :: Event String
-  bTimer <- stepper "" $ head <$> unions [eTimerFormatted, never] :: UI (Behavior String)
-  element timePanel # sink text bTimer
-  element timePanel # sink UI.class_ (maybe "hide" (const "") <$> bListBox)
-  UI.start timer
+  bTimer <- stepper "" eTimerFormatted
+  widget timePanel # sink text bTimer
+  widget timePanel # sink UI.class_ (maybe "hide" (const "") <$> bListBox)
+
+  onEvent eSudokuCorrect $ const $ UI.stop timer
+  onEvent eSudoku $ const $ UI.start timer
+
+  -- Solved Panel
+  solvedPanel <- UI.h3
+  let bSolved = (\x -> if x>28 then "Gewonnen!" else "") <$> bSudokuCorrect 
+  widget solvedPanel # sink text bSolved
   
   -- add panels to body
-  getBody w # set children ([captionPanel, choosePanel, contentPanel] ++ selects ++ [mistakesPanel, timePanel])
+  getBody w # set children ([captionPanel, choosePanel, contentPanel] ++ selects ++ [errorPanel, timePanel, solvedPanel])
 
 
-createCell :: Int -> Int -> Behavior (Maybe Digit) -> Event String -> UI (Element, Event Bool)
-createCell x y digit eSudoku =
+createCell :: Int -> Int -> Event String -> Event (Maybe Digit) -> UI (Element, Event Bool, Event Bool)
+createCell x y eSudoku eNewDigit =
   let classes = "cell row"++show y++"col"++show x
-      isFree digit' = case digit' of { (Guess _) -> True ; _ -> False}
+      isFree digit' = case digit' of { (Guess _) -> False ; _ -> True}
       digitString digit' = case digit' of
         (Guess _) -> ""
         (Free a) -> show a 
       in
    do
+     digit <- stepper Nothing eNewDigit
      cell <- UI.div #. classes
      
      -- input element of the cell. it gets initialized with the behavior of only showing the digit if it s a (Free digit) and not a (Guess a)
      cellInput <- UI.entry (maybe "" digitString <$> digit) 
-     element cellInput # sink UI.enabled (maybe False isFree <$> digit) # set (attr "maxlength") "1"
-     element cell # set children [getElement cellInput]
+     element cellInput # sink UI.enabled (maybe False (not . isFree) <$> digit) # set (attr "maxlength") "1"
+     cell # set' children [getElement cellInput]
      let bDigit = UI.userText cellInput
          eInput'' = rumors bDigit
 
-     eInput' <- stepper "0" eInput''
+     
+
+     eInput' <- stepper "0" $ head <$> unions [
+       "0" <$ eNewDigit,
+       eInput''
+       ]
      -- eInput' is the event stream of all user inputs to this cell
      -- eInput is the event stream of all user inputs to this cell which are different than the previous one
      let bInput''= fmap not <$> (==) <$> eInput' :: Behavior (String -> Bool)
@@ -222,9 +248,10 @@ createCell x y digit eSudoku =
          -- eDigitInput is event stream of all digit inputs (which are different than the previous one)
          eDigitInput = read <$> filterE isDigit' eInput :: Event Int
          eNonDigitInput = filterE (not . isDigit') eInput
-         eCorrectnessState = (\x y -> maybe False (either (==y) (const True) . toEither) x) <$> digit <@> eDigitInput :: Event Bool
-         eCorrect = () <$ filterE id eCorrectnessState
-         eNotCorrect = () <$ filterE not eCorrectnessState
+         eCorrectnessOfInput = (\x y -> maybe False (either (\z -> maybe False (\a -> z == a) y) (const True) . toEither) x) <$> digit <@> (maybeRead <$> eInput) :: Event Bool
+         eCorrectnessOfDigitInput = (\x y -> maybe False (either (==y) (const True) . toEither) x) <$> digit <@> eDigitInput :: Event Bool
+         eCorrect = () <$ filterE id eCorrectnessOfDigitInput
+         eNotCorrect = () <$ filterE not eCorrectnessOfDigitInput
          
          eInputClasses = head <$> unions [
            classes ++ " wrong" <$ eNotCorrect, -- add class 'wrong' when an incorrect digit is the guess
@@ -235,13 +262,22 @@ createCell x y digit eSudoku =
        classes <$ eSudoku, -- when a new sudoku gets selected remove the wrong/right classes
        eInputClasses
        ]
-     element cell # sink UI.class_ bInputClasses
+     widget cell # sink UI.class_ bInputClasses
+
+     let eCorrectness = head <$> unions [
+           maybe False isFree <$> eNewDigit,
+           eCorrectnessOfInput
+           ]
      
-     return (cell, eCorrectnessState)
+     return (cell, eCorrectnessOfDigitInput, eCorrectness)
+
+
+--- taken from Network-CGI-Protocol package
+maybeRead = fmap fst . listToMaybe . reads
   
 -- Separates the cells into 9 sudoku blocks and concatenates them
-sudokuGrid :: [UI Element] -> [UI Element]
-sudokuGrid cells =  concatMap (concat . blockify [[],[],[]]) $ chunksOf 27 cells
+sudokuGrid :: [Element] -> [UI Element]
+sudokuGrid cells =  concatMap (concat . blockify [[],[],[]]) $ chunksOf 27 $ map element cells
   where
     blockify :: [[UI Element]] -> [UI Element] -> [[UI Element]]
     blockify blocks [] = [ [UI.div #. "square" #+ cs] | cs <- blocks]
